4ca805b632b65c17f9a7c59e32e67a5ead0727a9
55soup <w2118@e-mirim.hs.kr>
2022-06-14 02:20:03 +0900

    main-js-주석추가

--- 연습파일/main/js/custom.js
+++ 연습파일/main/js/custom.js
@@ -1,6 +1,6 @@
 const btn_menu = document.querySelector(".btn_menu");
 const main_navbar = document.querySelector(".main_navbar");
-const close_menu = document.querySelector("#close_menu");
+// const close_menu = document.querySelector("#close_menu");
 
 btn_menu.addEventListener("click", showMenuBar);
 
@@ -8,5 +8,5 @@ function showMenuBar(event) {
   //클릭했을 때 이벤트
   event.preventDefault();
   main_navbar.classList.toggle("active");
-  close_menu.innerHTML = feather.icons["x"].toSvg();
+  // close_menu.innerHTML = feather.icons["x"].toSvg();
 }
