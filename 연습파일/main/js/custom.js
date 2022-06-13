9454f0c26d4c0c27330d5235f9938dcda30231b1
55soup <w2118@e-mirim.hs.kr>
2022-06-14 02:03:45 +0900

    헤더 nav반응형 구현 (완료)

---
+++ 연습파일/main/js/custom.js
@@ -0,0 +1,11 @@
+const btn_menu = document.querySelector(".btn_menu");
+const main_navbar = document.querySelector(".main_navbar");
+const close_menu = document.querySelector("#close_menu");
+
+btn_menu.addEventListener("click", showMenuBar);
+
+function showMenuBar(event) {
+  event.preventDefault();
+  main_navbar.classList.toggle("active");
+  close_menu.innerHTML = feather.icons["x"].toSvg();
+}
