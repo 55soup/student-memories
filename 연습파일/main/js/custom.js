3df62a55d9f6cd8bf0b6737a72eb1097c95118f8
55soup <w2118@e-mirim.hs.kr>
2022-06-14 02:17:08 +0900

    main-js-주석처리

--- 연습파일/main/js/custom.js
+++ 연습파일/main/js/custom.js
@@ -5,6 +5,7 @@ const close_menu = document.querySelector("#close_menu");
 btn_menu.addEventListener("click", showMenuBar);
 
 function showMenuBar(event) {
+  //클릭했을 때 이벤트
   event.preventDefault();
   main_navbar.classList.toggle("active");
   close_menu.innerHTML = feather.icons["x"].toSvg();
